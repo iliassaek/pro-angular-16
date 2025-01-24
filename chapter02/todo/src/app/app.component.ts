92188aa87dbef775fe2159c871c9d364feac202a
iliass <iliassaek.dev@gmail.com>
2025-01-24 18:34:17 +0100

    feat add data table

--- chapter02/todo/src/app/app.component.ts
+++ chapter02/todo/src/app/app.component.ts
@@ -41,4 +41,8 @@ export class AppComponent {
   get itemCount(): number {
     return this.list.items.filter((item) => !item.complete).length;
   }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
 }
