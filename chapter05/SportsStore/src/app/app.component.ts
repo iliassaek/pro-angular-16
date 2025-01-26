450e27f0a447e90c285ba36069604a9e494c9f59
iliass <iliassaek.dev@gmail.com>
2025-01-26 04:47:56 +0100

    feat: add providers

--- chapter05/SportsStore/src/app/app.component.ts
+++ chapter05/SportsStore/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductRepository } from './model/product.repository';
+import { StaticDataSource } from './model/static.datasource';
 
 @Component({
   selector: 'app',
   template: `<div class="bg-success p-2 text-center text-white">
                 This is SportsStore
               </div>`,
+  providers:[ProductRepository, StaticDataSource]
 })
 export class AppComponent {
   title = 'SportsStore';
