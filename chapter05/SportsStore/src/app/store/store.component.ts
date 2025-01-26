f03ed254befc7d15d946ec6c1541c49cf1476050
iliass <iliassaek.dev@gmail.com>
2025-01-26 06:01:18 +0100

    feat: product detail

--- chapter05/SportsStore/src/app/store/store.component.ts
+++ chapter05/SportsStore/src/app/store/store.component.ts
@@ -2,9 +2,10 @@ import { Component, Signal } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
 import { ModelModule } from '../model/model.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [ModelModule],
+  imports: [ModelModule,CommonModule],
   selector: 'store',
   templateUrl: 'store.component.html',
 })
