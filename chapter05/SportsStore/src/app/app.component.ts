dd1edc7481be2affaf455922719758bd02d7e78c
iliass <iliassaek.dev@gmail.com>
2025-01-26 04:57:27 +0100

    fix: declaring ModelModule

--- chapter05/SportsStore/src/app/app.component.ts
+++ chapter05/SportsStore/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { ProductRepository } from './model/product.repository';
-import { StaticDataSource } from './model/static.datasource';
 
 @Component({
   selector: 'app',
   template: `<div class="bg-success p-2 text-center text-white">
                 This is SportsStore
-              </div>`,
-  providers:[ProductRepository, StaticDataSource]
+              </div>`
 })
 export class AppComponent {
   title = 'SportsStore';
