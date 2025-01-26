be11eedc35a33592e3936f078b395e18cd68c97d
iliass <iliassaek.dev@gmail.com>
2025-01-26 09:41:28 +0100

    feat: pagination

--- chapter05/SportsStore/src/app/store/store.component.ts
+++ chapter05/SportsStore/src/app/store/store.component.ts
@@ -4,9 +4,8 @@ import { ProductRepository } from '../model/product.repository';
 import { ModelModule } from '../model/model.module';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
-  imports: [ModelModule,CommonModule],
+  imports: [ModelModule, CommonModule],
   selector: 'store',
   templateUrl: 'store.component.html',
 })
@@ -14,20 +13,49 @@ export class StoreComponent {
   products: Signal<Product[]>;
   categories: Signal<string[]>;
   selectedCategory = signal<string | undefined>(undefined);
+  productsPerPage = signal(4);
+  selectedPage = signal(1);
+  pagedProducts: Signal<Product[]>;
+  pageNumbers: Signal<number[]>;
 
-  constructor(private repository: ProductRepository) { 
-      this.products = computed(() => {
-          if (this.selectedCategory() == undefined) {
-              return this.repository.products();
-          } else {
-              return this.repository.products().filter(p => 
-                  p.category === this.selectedCategory());
-          }
-      })
-      this.categories = repository.categories;
+  constructor(private repository: ProductRepository) {
+    this.products = computed(() => {
+      if (this.selectedCategory() == undefined) {
+        return this.repository.products();
+      } else {
+        return this.repository
+          .products()
+          .filter((p) => p.category === this.selectedCategory());
+      }
+    });
+    this.categories = repository.categories;
+    let pageIndex = computed(() => {
+      return (this.selectedPage() - 1) * this.productsPerPage();
+    });
+    this.pagedProducts = computed(() => {
+      return this.products().slice(
+        pageIndex(),
+        pageIndex() + this.productsPerPage()
+      );
+    });
+    this.pageNumbers = computed(() => {
+      return Array(Math.ceil(this.products().length / this.productsPerPage()))
+        .fill(0)
+        .map((x, i) => i + 1);
+    });
   }
 
   changeCategory(newCategory?: string) {
-      this.selectedCategory.set(newCategory);
+    this.selectedCategory.set(newCategory);
+    this.changePage(1);
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage.set(newPage);
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage.set(Number(newSize));
+    this.changePage(1);
   }
 }
