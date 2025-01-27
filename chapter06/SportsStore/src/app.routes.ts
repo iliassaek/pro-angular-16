880e7ca352fd238a0c977c83f7124deb7931eec5
iliass <iliassaek.dev@gmail.com>
2025-01-27 19:45:29 +0100

    feat: Guarding routes

--- chapter06/SportsStore/src/app.routes.ts
+++ chapter06/SportsStore/src/app.routes.ts
@@ -2,10 +2,19 @@ import { Routes } from '@angular/router';
 import { CartDetailComponent } from './app/store/cartDetail.component';
 import { CheckoutComponent } from './app/store/checkout.component';
 import { StoreComponent } from './app/store/store.component';
+import { StoreFirstGuard } from './app/storeFirst.guard';
 
 export const routes: Routes = [
-  { path: 'store', component: StoreComponent },
-  { path: 'cart', component: CartDetailComponent },
-  { path: 'checkout', component: CheckoutComponent },
+  { path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard] },
+  {
+    path: 'cart',
+    component: CartDetailComponent,
+    canActivate: [StoreFirstGuard],
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    canActivate: [StoreFirstGuard],
+  },
   { path: '**', redirectTo: '/store' },
 ];
--- chapter06/SportsStore/src/app/app.component.ts
+++ chapter06/SportsStore/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { StoreComponent } from './store/store.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports:[RouterOutlet],
+  imports: [RouterOutlet],
   selector: 'app',
-  template: `
-  <router-outlet></router-outlet>
-  `,
+  template: ` <router-outlet></router-outlet> `,
 })
 export class AppComponent {
   title = 'SportsStore';
