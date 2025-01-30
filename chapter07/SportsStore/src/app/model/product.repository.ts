e24d7e2cec1f75db7d43e00cf9bf36e283fc9165
iliass <iliassaek.dev@gmail.com>
2025-01-30 05:54:37 +0100

    feat: extending data source andrepository

--- chapter07/SportsStore/src/app/model/product.repository.ts
+++ chapter07/SportsStore/src/app/model/product.repository.ts
@@ -1,26 +1,62 @@
-import { Injectable, Signal, computed } from "@angular/core";
+import { computed, Injectable, Signal, signal } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+
 import { Product } from "./product.model";
 import { RestDataSource } from "./rest.datasource";
-import { toSignal} from "@angular/core/rxjs-interop";
 
 @Injectable()
 export class ProductRepository {
-    products: Signal<Product[]>;
-    categories: Signal<string[]>;
+  products = signal<Product[]>([]);
+  categories: Signal<string[]>;
 
-    constructor(private dataSource: RestDataSource) {
-        this.products = toSignal(dataSource.products, {
-            initialValue: []
+  constructor(private dataSource: RestDataSource) {
+    this.dataSource.products.subscribe(data => {
+      this.products.set(data);
+    });
+
+    this.categories = computed(() => {
+      return this.products()
+        .map(p => p.category ?? "(None)")
+        .filter((c, index, array) => array.indexOf(c) === index)
+        .sort();
+    });
+  }
+
+  getProduct(id: number): Product | undefined {
+    return this.products().find(p => p.id === id);
+  }
+
+  saveProduct(product: Product) {
+    if (product.id == null || product.id === 0) {
+      this.dataSource.saveProduct(product).subscribe(p => {
+        // Use 'update' instead of 'mutate'
+        this.products.update(list => {
+          list.push(p);       // Imperatively mutate the array
+          return list;        // Return the updated array
         });
-        this.categories = computed(() => {
-            return this.products()
-                .map(p => p.category ?? "(None)")
-                .filter((c, index, array) => 
-                    array.indexOf(c) == index).sort();
-        })
+      });
+    } else {
+      this.dataSource.updateProduct(product).subscribe(() => {
+        this.products.update(list => {
+          const idx = list.findIndex(existing => existing.id === product.id);
+          if (idx !== -1) {
+            list.splice(idx, 1, product);
+          }
+          return list;
+        });
+      });
     }
+  }
 
-    getProduct(id: number): Product | undefined {
-        return this.products().find(p => p.id == id);
-    }
+  deleteProduct(id: number) {
+    this.dataSource.deleteProduct(id).subscribe(() => {
+      this.products.update(list => {
+        const idx = list.findIndex(p => p.id === id);
+        if (idx !== -1) {
+          list.splice(idx, 1);
+        }
+        return list;
+      });
+    });
+  }
 }
