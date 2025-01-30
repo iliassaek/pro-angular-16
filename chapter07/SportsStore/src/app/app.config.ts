71393e45034556317bf04d21d00d396b39fc4474
iliass <iliassaek.dev@gmail.com>
2025-01-30 06:02:30 +0100

    chore: install angular material

--- chapter07/SportsStore/src/app/app.config.ts
+++ chapter07/SportsStore/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { routes } from '../app.routes';
 import { provideRouter } from '@angular/router';
 import { StoreFirstGuard } from './storeFirst.guard';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     StoreFirstGuard,
-    provideHttpClient()
+    provideHttpClient(), provideAnimationsAsync()
   ],
 };
