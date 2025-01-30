a84c59f9e6fa8f5319c5f54930c14c6a044c305d
iliass <iliassaek.dev@gmail.com>
2025-01-30 06:42:29 +0100

    feat: implementing admin board

--- chapter07/SportsStore/src/app/admin/admin.component.ts
+++ chapter07/SportsStore/src/app/admin/admin.component.ts
@@ -1,6 +1,18 @@
-import { Component } from "@angular/core";
- 
+import { Component } from '@angular/core';
+import { MaterialFeatures } from './material.module';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from '../model/auth.service';
+import { CommonModule } from '@angular/common';
+
 @Component({
-    templateUrl: "admin.component.html"
+  imports: [MaterialFeatures, RouterOutlet, CommonModule, RouterModule],
+  templateUrl: 'admin.component.html',
 })
-export class AdminComponent {}
+export class AdminComponent {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  logout() {
+    this.auth.clear();
+    this.router.navigateByUrl('/');
+  }
+}
--- chapter07/SportsStore/src/app/admin/admin.module.ts
+++ chapter07/SportsStore/src/app/admin/admin.module.ts
@@ -6,15 +6,36 @@ import { AuthComponent } from './auth.component';
 import { AdminComponent } from './admin.component';
 import { AuthGuard } from './auth.guard';
 import { MaterialFeatures } from './material.module';
+import { ProductEditorComponent } from './productEditor.component';
+import { ProductTableComponent } from './productTable.component';
+import { OrderTableComponent } from './orderTable.component';
 
 let routing = RouterModule.forChild([
   { path: 'auth', component: AuthComponent },
-  { path: "main", component: AdminComponent, canActivate: [AuthGuard] },
+  {
+    path: 'main',
+    component: AdminComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'products/:mode/:id', component: ProductEditorComponent },
+      { path: 'products/:mode', component: ProductEditorComponent },
+      { path: 'products', component: ProductTableComponent },
+      { path: 'orders', component: OrderTableComponent },
+      { path: '**', redirectTo: 'products' },
+    ],
+  },
   { path: '**', redirectTo: 'auth' },
 ]);
 
 @NgModule({
-  imports: [CommonModule, FormsModule, routing, AuthComponent, AdminComponent, MaterialFeatures],
-  providers:[AuthGuard]
+  imports: [
+    CommonModule,
+    FormsModule,
+    routing,
+    AuthComponent,
+    AdminComponent,
+    MaterialFeatures,
+  ],
+  providers: [AuthGuard],
 })
 export class AdminModule {}
