f00e8a1d9984d37bc6a0d682b0f581d49c15d4f2
iliass <iliassaek.dev@gmail.com>
2025-01-30 09:20:32 +0100

    feat: add a product

--- chapter07/SportsStore/src/app/admin/productEditor.component.ts
+++ chapter07/SportsStore/src/app/admin/productEditor.component.ts
@@ -1,8 +1,33 @@
 import { Component } from "@angular/core";
- 
+import { Router, ActivatedRoute, RouterModule } from "@angular/router";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+import { CommonModule } from "@angular/common";
+import { MaterialFeatures } from "./material.module";
+import { FormsModule } from "@angular/forms";
+import { ModelModule } from "../model/model.module";
+
 @Component({
-    template: `<h3 style="padding-top: 10px">
-                Product Editor Placeholder
-              </h3>`
+    imports:[CommonModule, MaterialFeatures, FormsModule, ModelModule, RouterModule],
+    templateUrl: "productEditor.component.html"
 })
-export class ProductEditorComponent { }
+export class ProductEditorComponent { 
+    editing: boolean = false;
+    product: Product = new Product();
+
+    constructor(private repository: ProductRepository,
+                private router: Router,
+                activeRoute: ActivatedRoute) {
+
+        this.editing = activeRoute.snapshot.params["mode"] == "edit";
+        if (this.editing) {
+            Object.assign(this.product,
+                repository.getProduct(activeRoute.snapshot.params["id"]));
+        }
+    }
+
+    save() {
+        this.repository.saveProduct(this.product);
+        this.router.navigateByUrl("/admin/main/products");
+    }
+}
