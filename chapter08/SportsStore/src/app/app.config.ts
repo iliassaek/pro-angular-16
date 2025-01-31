56763aa7708d3f66b38266885d6d3ce50b766cb5
iliass <iliassaek.dev@gmail.com>
2025-01-31 07:01:40 +0100

    feat: add pwa pckage

--- chapter08/SportsStore/src/app/app.config.ts
+++ chapter08/SportsStore/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { routes } from '../app.routes';
 import { provideRouter } from '@angular/router';
 import { StoreFirstGuard } from './storeFirst.guard';
@@ -6,12 +6,16 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { PlatformService } from './store/plateform.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     StoreFirstGuard,
-    provideHttpClient(), provideAnimationsAsync(), provideClientHydration(withEventReplay()), PlatformService
+    provideHttpClient(), provideAnimationsAsync(), provideClientHydration(withEventReplay()), PlatformService, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 };
