04dfb36e63ff6c8439905c12e5d66bd5253aaa2c
iliass <iliassaek.dev@gmail.com>
2025-02-01 10:29:31 +0100

    feat: writable signal

--- chapter10/example/src/app/product/product.component.ts
+++ chapter10/example/src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Model } from './repository.model';
 
 @Component({
@@ -9,41 +9,44 @@ import { Model } from './repository.model';
 })
 export class ProductComponent {
   private model: Model = new Model();
-  private messages = ["Total", "Price"];
-  private index = 0;
+  private messages = ['Total', 'Price'];
+  private index = signal<number>(0);
 
   get count(): number {
-      let result =  this.model.getProducts().length;
-      // let total = 0;
-      // for (let i = 0; i < 1000000000; i++) {
-      //     total += 1;
-      // }
-      console.log(`count value read: ${result}`);
-      return result;
+    let result = this.model.getProducts().length;
+    // let total = 0;
+    // for (let i = 0; i < 1000000000; i++) {
+    //     total += 1;
+    // }
+    console.log(`count value read: ${result}`);
+    return result;
   }
 
   get total(): string {
-      let result = this.model.getProducts()
-          .reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2);
-      console.log(`total value read: ${result}`);            
-      return result;
+    let result = this.model
+      .getProducts()
+      .reduce((total, p) => total + (p.price ?? 0), 0)
+      .toFixed(2);
+    console.log(`total value read: ${result}`);
+    return result;
   }
 
   get message(): string {
-      let result = `${this.messages[this.index]} $${this.total}`;
-      console.log(`message value read: ${result}`);        
-      return result;
+    let result = `${this.messages[this.index()]} $${this.total}`;
+    console.log(`message value read: ${result}`);
+    return result;
   }
 
   toggleMessage() {
-      console.clear();        
-      console.log("toggleMessage method invoked");
-      this.index = (this.index + 1) % 2;
+    console.clear();
+    console.log('toggleMessage method invoked');
+    //this.index = (this.index + 1) % 2;
+    this.index.update((currentVal) => (currentVal + 1) % 2);
   }
 
   removeProduct() {
-      console.clear();
-      console.log("removeProduct method invoked");
-      this.model.deleteProduct(this.model.getProducts()[0].id ?? 0);
+    console.clear();
+    console.log('removeProduct method invoked');
+    this.model.deleteProduct(this.model.getProducts()[0].id ?? 0);
   }
 }
