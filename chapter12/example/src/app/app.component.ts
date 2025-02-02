7f5d050e611b15fc3c692a29dee0027e9c2cb033
iliass <iliassaek.dev@gmail.com>
2025-02-02 11:53:30 +0100

    chore: init chapter 12

---
+++ chapter12/example/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from "./product/product.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [ProductComponent, ProductComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'example';
+}
---
+++ chapter12/example/src/app/product/product.component.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
+import { Component, computed } from '@angular/core';
+import { Model } from './repository.model';
+
+import { Product } from './product.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product',
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent {
+  private model: Model = new Model();
+
+  products = computed<Product[]>(() => this.model.Products());
+
+  count = computed<number>(() => this.products().length);
+
+  classes = computed<string>(() =>
+    this.count() == 5 ? 'bg-success' : 'bg-warning'
+  );
+
+  getClasses(key: number) {
+    return (
+      'p-2 ' +
+      ((this.products()[key].price ?? 0) > 50 ? 'bg-info' : 'bg-warning')
+    );
+  }
+
+  getClassMap(key: number): Object {
+    let product = this.products()[key];
+    return {
+      'text-center bg-danger': product.name == 'Kayak',
+      'bg-info': (product.price ?? 0) < 50,
+    };
+  }
+
+  fontSizeWithUnits: string = '30px';
+  fontSizeWithoutUnits: string = '30';
+
+  getStyles(key: number) {
+    return {
+      fontSize: '30px',
+      'margin.px': 100,
+      color: (this.products()[key].price ?? 0) > 50 ? 'red' : 'green',
+    };
+  }
+}
