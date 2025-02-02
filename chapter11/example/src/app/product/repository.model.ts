2fdd8741cf55142810de0f7c51f835cd768517d6
iliass <iliassaek.dev@gmail.com>
2025-02-02 06:42:42 +0100

    chore: preparing chapter 11

---
+++ chapter11/example/src/app/product/repository.model.ts
@@ -0,0 +1,46 @@
+import { Signal, signal, WritableSignal } from '@angular/core';
+import { SimpleDataSource } from './datasource.model';
+import { Product } from './product.model';
+
+export class Model {
+  private dataSource: SimpleDataSource;
+  private products: WritableSignal<Product[]>;
+  private locator = (p: Product, id: number | any) => p.id == id;
+
+  constructor() {
+    this.dataSource = new SimpleDataSource();
+    this.products = signal(new Array<Product>());
+    this.products.update((prods) => [...prods, ...this.dataSource.getData()]);
+  }
+
+  get Products(): Signal<Product[]> {
+    return this.products.asReadonly();
+  }
+
+  getProduct(id: number): Product | undefined {
+    return this.products().find((p) => this.locator(p, id));
+  }
+
+  saveProduct(product: Product) {
+    if (product.id == 0 || product.id == undefined) {
+      product.id = this.generateID();
+      this.products.update((prods) => [...prods, product]);
+    } else {
+      this.products.update((prods) =>
+        prods.map((p) => (this.locator(p, product.id) ? product : p))
+      );
+    }
+  }
+
+  deleteProduct(id: number) {
+    this.products.update((prods) => prods.filter((p) => !this.locator(p, id)));
+  }
+
+  private generateID(): number {
+    let candidate = 100;
+    while (this.getProduct(candidate) != null) {
+      candidate++;
+    }
+    return candidate;
+  }
+}
