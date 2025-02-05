073a7f2b9ea9717fb6eb6d58775dc1cf07c65906
iliass <iliassaek.dev@gmail.com>
2025-02-05 19:58:55 +0100

    chore: set up chapter 13

--- chapter13/example/src/app/product/product.component.ts
+++ chapter13/example/src/app/product/product.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 /* eslint-disable @typescript-eslint/no-wrapper-object-types */
-import { Component, computed } from '@angular/core';
+import { Component, computed, signal } from "@angular/core";
 import { Model } from './repository.model';
 
 import { Product } from './product.model';
@@ -14,29 +14,14 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductComponent {
   private model: Model = new Model();
-
-  targetName: string = 'Kayak';
-
+ 
   products = computed<Product[]>(() => this.model.Products());
 
   count = computed<number>(() => this.products().length);
 
   product(key: number): Product | undefined {
-    return this.model.getProduct(key);
+      return this.model.getProduct(key);
   }
 
-  removeProduct() {
-    this.model.deleteProduct(this.model.Products()[0].id ?? 0);
-  }
-
-  swapProduct() {
-    let p = this.products()[0];
-    if (p != null && p.id != null) {
-      this.model.deleteProduct(p.id);
-      this.model.saveProduct({ ...p, id: 0 });
-    }
-  }
-  getKey(index: number, product: Product) {
-    return product.name;
-  }
+  selectedProduct = signal<string | undefined>(undefined);
 }
