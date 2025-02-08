7e6253c378ce8c7c0f4582dcd10a5f9de0d7cbbe
iliass <iliassaek.dev@gmail.com>
2025-02-08 05:23:27 +0100

    feat: handling validation errors in ts

--- chapter13/example/src/app/product/product.component.ts
+++ chapter13/example/src/app/product/product.component.ts
@@ -5,7 +5,7 @@ import { Model } from './repository.model';
 
 import { Product } from './product.model';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
@@ -47,5 +47,31 @@ export class ProductComponent {
 
   addProduct(p: Product) {
     console.log('New Product: ' + this.jsonProduct);
+  }
+
+  getMessages(errs: ValidationErrors | null, name: string): string[] {
+    let messages: string[] = [];
+    for (let errorName in errs) {
+      switch (errorName) {
+        case 'required':
+          messages.push(`You must enter a ${name}`);
+          break;
+        case 'minlength':
+          messages.push(`A ${name} must be at least
+                    ${errs['minlength'].requiredLength}
+                    characters`);
+          break;
+        case 'pattern':
+          messages.push(`The ${name} contains
+                     illegal characters`);
+          break;
+      }
+    }
+    return messages;
+  }
+
+  getValidationMessages(state: NgModel, thingName?: string) {
+    let thing: string = state.path?.[0] ?? thingName;
+    return this.getMessages(state.errors, thing);
   }
 }
