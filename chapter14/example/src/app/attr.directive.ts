8c66643f4aa2b594ceaa1985c80b25f8adf9c788
iliass <iliassaek.dev@gmail.com>
2025-02-08 09:45:10 +0100

    feat: reading host element attributes

--- chapter14/example/src/app/attr.directive.ts
+++ chapter14/example/src/app/attr.directive.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @angular-eslint/directive-selector */
-import { Directive, ElementRef } from "@angular/core";
- 
+import { Attribute, Directive, ElementRef } from '@angular/core';
+
 @Directive({
-    selector: "[pa-attr]",
+  selector: '[pa-attr]',
 })
 export class PaAttrDirective {
-    constructor(element: ElementRef) {
-        element.nativeElement.classList.add("table-success", "fw-bold");
-    }
+  constructor(element: ElementRef, @Attribute('pa-attr-class') bgClass: string) {
+    element.nativeElement.classList.add(bgClass || 'table-success', 'fw-bold');
+  }
 }
