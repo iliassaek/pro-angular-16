41fa272114546e99bba2c0949de9377bf0e6573c
iliass <iliassaek.dev@gmail.com>
2025-02-09 05:06:18 +0100

    feat: host element binding

--- chapter14/example/src/app/attr.directive.ts
+++ chapter14/example/src/app/attr.directive.ts
@@ -3,49 +3,45 @@
 /* eslint-disable @angular-eslint/no-input-rename */
 /* eslint-disable @angular-eslint/use-lifecycle-interface */
 /* eslint-disable @angular-eslint/directive-selector */
-import {  Directive, ElementRef, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import {  Directive, ElementRef, EventEmitter, HostBinding, HostListener, Input, Output, SimpleChanges } from '@angular/core';
 import { Product } from './product/product.model';
 
 @Directive({
   selector: '[pa-attr]',
 })
 export class PaAttrDirective {
-  
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.addEventListener("click", () => {
-        if (this.product != null) {
-            this.click.emit(this.product.category);
-        }
-    });
-}
+  constructor(private element: ElementRef) {}
 
   @Input('pa-attr')
+  @HostBinding('class')
   bgClass: string | null = '';
 
   @Input({ required: true, alias: 'fg-class' })
   fgClass: string | undefined;
 
-  @Input("pa-product")
+  @Input('pa-product')
   product: Product = new Product();
 
-  @Output("pa-category")
+  @Output('pa-category')
   click = new EventEmitter<string>();
 
   ngOnChanges(changes: SimpleChanges) {
-    ['bgClass', 'fgClass'].forEach((c) => {
-      let change = changes[c];
-      if (change) {
-        let classList = this.element.nativeElement.classList;
-        if (
-          !change.isFirstChange() &&
-          classList.contains(change.previousValue)
-        ) {
-          classList.remove(change.previousValue);
-        }
-        if (!classList.contains(change.currentValue)) {
-          classList.add(change.currentValue);
-        }
+    let change = changes['fgClass'];
+    if (change) {
+      let classList = this.element.nativeElement.classList;
+      if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+        classList.remove(change.previousValue);
       }
-    });
+      if (!classList.contains(change.currentValue)) {
+        classList.add(change.currentValue);
+      }
+    }
+  }
+
+  @HostListener('click')
+  triggerCustomEvent() {
+    if (this.product != null) {
+      this.click.emit(this.product.category);
+    }
   }
 }
