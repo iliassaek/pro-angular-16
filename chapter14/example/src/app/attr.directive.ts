ed067e901ee0c69b058f88b60607e414f337ebea
iliass <iliassaek.dev@gmail.com>
2025-02-09 04:39:12 +0100

    feat: create custom events

--- chapter14/example/src/app/attr.directive.ts
+++ chapter14/example/src/app/attr.directive.ts
@@ -1,19 +1,35 @@
+/* eslint-disable @angular-eslint/no-output-rename */
+/* eslint-disable @angular-eslint/no-output-native */
 /* eslint-disable @angular-eslint/no-input-rename */
 /* eslint-disable @angular-eslint/use-lifecycle-interface */
 /* eslint-disable @angular-eslint/directive-selector */
-import {  Directive, ElementRef, Input, SimpleChanges } from '@angular/core';
+import {  Directive, ElementRef, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { Product } from './product/product.model';
 
 @Directive({
   selector: '[pa-attr]',
 })
 export class PaAttrDirective {
-  constructor(private element: ElementRef) {}
+  
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.addEventListener("click", () => {
+        if (this.product != null) {
+            this.click.emit(this.product.category);
+        }
+    });
+}
 
   @Input('pa-attr')
   bgClass: string | null = '';
 
   @Input({ required: true, alias: 'fg-class' })
   fgClass: string | undefined;
+
+  @Input("pa-product")
+  product: Product = new Product();
+
+  @Output("pa-category")
+  click = new EventEmitter<string>();
 
   ngOnChanges(changes: SimpleChanges) {
     ['bgClass', 'fgClass'].forEach((c) => {
