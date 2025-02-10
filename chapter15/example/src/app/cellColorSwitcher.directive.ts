2f314ea4bdedce605a934d073215645e421771e8
iliass <iliassaek.dev@gmail.com>
2025-02-10 17:43:26 +0100

    feat: querying multiple content children

--- chapter15/example/src/app/cellColorSwitcher.directive.ts
+++ chapter15/example/src/app/cellColorSwitcher.directive.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @angular-eslint/use-lifecycle-interface */
 /* eslint-disable @angular-eslint/directive-selector */
 /* eslint-disable @angular-eslint/directive-class-suffix */
 /* eslint-disable @angular-eslint/no-input-rename */
 /* eslint-disable @typescript-eslint/no-wrapper-object-types */
-import { Directive, Input, SimpleChanges, ContentChild } from '@angular/core';
+import { Directive, Input, SimpleChanges, ContentChildren, QueryList } from '@angular/core';
 import { PaCellColor } from './cellcolor.directive';
+
 
 @Directive({
   selector: 'table',
@@ -12,12 +14,22 @@ export class PaCellColorSwitcher {
   @Input('paCellDarkColor')
   modelProperty: Boolean | undefined;
 
-  @ContentChild(PaCellColor)
-  contentChild: PaCellColor | undefined;
+  @ContentChildren(PaCellColor, {descendants: true})    
+  contentChildren: QueryList<PaCellColor> | undefined;
 
   ngOnChanges(changes: SimpleChanges) {
-    if (this.contentChild != null) {
-      this.contentChild.setColor(changes['modelProperty'].currentValue);
-    }
+      this.updateContentChildren(changes["modelProperty"].currentValue);
+  }
+
+  private updateContentChildren(dark: Boolean) {
+      if (this.contentChildren != null && dark != undefined) {
+          this.contentChildren.forEach((child, index) => {
+              if (dark) {
+                  child.setColor(index % 2 ? dark : !dark);
+              } else {
+                  child.setColor(false);
+              }
+          });
+      }
   }
 }
