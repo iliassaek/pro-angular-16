0f30b88bcedb3b56ea6a7396b65d4806a5714cc0
iliass <iliassaek.dev@gmail.com>
2025-02-11 10:39:49 +0100

    feat: using output properties to coordinate components

--- chapter16/example/src/app/productForm.component.ts
+++ chapter16/example/src/app/productForm.component.ts
@@ -1,12 +1,26 @@
+/* eslint-disable @angular-eslint/no-output-rename */
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 /* eslint-disable @angular-eslint/component-selector */
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Product } from "./product/product.model";
+import { FormsModule } from "@angular/forms";
  
 @Component({
     selector: "paProductForm",
-    template: "<div>{{model}}</div>"
+    templateUrl: "productForm.component.html",
+    imports:[
+        FormsModule
+    ]
 })
 export class ProductFormComponent {
+    newProduct: Product = new Product();
 
-    model: string = "This is the model";
+    @Output("paNewProduct")
+    newProductEvent = new EventEmitter<Product>();
+
+    submitForm(form: any) {
+        this.newProductEvent.emit(this.newProduct);
+        this.newProduct = new Product();
+        form.resetForm();
+    }
 }
