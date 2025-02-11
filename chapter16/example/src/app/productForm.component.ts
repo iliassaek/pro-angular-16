05bdcdb94a2ac88d7595442bc1f39509159080ac
iliass <iliassaek.dev@gmail.com>
2025-02-11 06:50:33 +0100

    feat: add property

--- chapter16/example/src/app/productForm.component.ts
+++ chapter16/example/src/app/productForm.component.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 /* eslint-disable @angular-eslint/component-selector */
 import { Component } from "@angular/core";
  
 @Component({
     selector: "paProductForm",
-    template: "<div>This is the form component</div>"
+    template: "<div>{{model}}</div>"
 })
 export class ProductFormComponent {
- 
+
+    model: string = "This is the model";
 }
