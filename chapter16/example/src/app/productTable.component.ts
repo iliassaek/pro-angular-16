4f9bd3af481ace33e522dbb389c764cfb0735760
iliass <iliassaek.dev@gmail.com>
2025-02-11 06:48:33 +0100

    feat: external template

--- chapter16/example/src/app/productTable.component.ts
+++ chapter16/example/src/app/productTable.component.ts
@@ -1,12 +1,8 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component } from "@angular/core";
- 
-@Component({
-    selector: "paProductTable",
-    template: `<div class='bg-info p-2'>
-                   This is a multiline template
-               </div>`
-})
-export class ProductTableComponent {
+import { Component } from '@angular/core';
 
-}
+@Component({
+  selector: 'paProductTable',
+  templateUrl: 'productTable.component.html',
+})
+export class ProductTableComponent {}
