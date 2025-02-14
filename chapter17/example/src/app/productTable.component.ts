df92ebe6412d463af3c3e3725ae9c1fec4a52115
iliass <iliassaek.dev@gmail.com>
2025-02-14 06:22:34 +0100

    feat: selecting value

--- chapter17/example/src/app/productTable.component.ts
+++ chapter17/example/src/app/productTable.component.ts
@@ -20,32 +20,34 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     PaAddTaxPipe,
     PaCategoryFilterPipe,
-    FormsModule
-  ]
+    FormsModule,
+  ],
 })
 export class ProductTableComponent {
-  @Input({ alias: "model", required: true})
+  @Input({ alias: 'model', required: true })
   dataModel!: Model;
 
   constructor(private changeRef: ChangeDetectorRef) {}
-  
+
   get Products(): Signal<Product[]> {
-      return this.dataModel.Products;
+    return this.dataModel.Products;
   }
 
   getProduct(key: number): Product | undefined {
-      return this.dataModel?.getProduct(key);
+    return this.dataModel?.getProduct(key);
   }
 
   deleteProduct(key: number) {
-      this.dataModel.deleteProduct(key);
+    this.dataModel.deleteProduct(key);
   }
 
   taxRate: number = 0;
   categoryFilter: string | undefined;
   itemCount: number = 3;
 
-  dateObject: Date = new Date(2020, 1, 20);
-  dateString: string = "2020-02-20T00:00:00.000Z";
-  dateNumber: number = 1582156800000;
+  selectMap = {
+    Watersports: 'stay dry',
+    Soccer: 'score goals',
+    other: 'have fun',
+  };
 }
