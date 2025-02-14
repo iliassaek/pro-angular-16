d6af646eb7fb16cfaa7f4bd5c4b9a849851040d8
iliass <iliassaek.dev@gmail.com>
2025-02-14 04:47:00 +0100

    feat: format to date

--- chapter17/example/src/app/productTable.component.ts
+++ chapter17/example/src/app/productTable.component.ts
@@ -44,4 +44,8 @@ export class ProductTableComponent {
   taxRate: number = 0;
   categoryFilter: string | undefined;
   itemCount: number = 3;
+
+  dateObject: Date = new Date(2020, 1, 20);
+  dateString: string = "2020-02-20T00:00:00.000Z";
+  dateNumber: number = 1582156800000;
 }
