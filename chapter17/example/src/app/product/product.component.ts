d523b2369fbe2bbd7cdc258177fecab0cef658c6
iliass <iliassaek.dev@gmail.com>
2025-02-14 04:14:30 +0100

    feat: format number to french

--- chapter17/example/src/app/product/product.component.ts
+++ chapter17/example/src/app/product/product.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 /* eslint-disable @typescript-eslint/no-wrapper-object-types */
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { Model } from './repository.model';
 
 import { Product } from './product.model';
@@ -9,6 +9,10 @@ import { FormsModule } from '@angular/forms';
 import { ProductFormComponent } from '../productForm.component';
 import { ProductTableComponent } from '../productTable.component';
 import { PaToggleView } from '../toggleView.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr, 'fr-FR');
 
 @Component({
   selector: 'app-product',
@@ -21,6 +25,7 @@ import { PaToggleView } from '../toggleView.component';
   ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }]
 })
 export class ProductComponent {
   model: Model = new Model();
