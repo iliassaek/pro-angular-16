474a399db0121b6ec57ddd3634ac6f5115ec0515
iliass <iliassaek.dev@gmail.com>
2025-02-15 09:56:46 +0100

    feat: declare app module

--- chapter20/exampleApp/src/app/app.component.ts
+++ chapter20/exampleApp/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
