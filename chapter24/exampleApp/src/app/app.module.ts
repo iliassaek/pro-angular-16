6aa9b158d10ff6b14b9d75f79e3653e869d9ba8c
iliass <iliassaek.dev@gmail.com>
2025-02-20 09:22:33 +0100

    feat: creating routing configuration

--- chapter24/exampleApp/src/app/app.module.ts
+++ chapter24/exampleApp/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ModelModule } from "./model/model.module";
-import { CoreModule } from "./core/core.module";
-import { TableComponent } from "./core/table.component";
-import { FormComponent } from "./core/form.component";
-import { MessageModule } from "./messages/message.module";
-import { MessageComponent } from "./messages/message.component";
+import { ModelModule } from './model/model.module';
+import { CoreModule } from './core/core.module';
+import { MessageModule } from './messages/message.module';
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [],
-  imports: [BrowserModule, ModelModule, CoreModule, MessageModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ModelModule, CoreModule, MessageModule, routing],
   providers: [],
-  bootstrap: [TableComponent, FormComponent, MessageComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
