724f30a0c8000f7a374dd79c4f3915209f4d151e
iliass <iliassaek.dev@gmail.com>
2025-02-22 06:37:07 +0100

    feat: using redirection route

--- chapter25/exampleApp/src/app/app.routing.ts
+++ chapter25/exampleApp/src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { Routes, RouterModule } from "@angular/router";
 import { TableComponent } from "./core/table.component";
 import { FormComponent } from "./core/form.component";
+import { NotFoundComponent } from "./core/notFoundComponent";
  
 const routes: Routes = [
   { path: "form/:mode/:id", component: FormComponent },
   { path: 'form/:mode', component: FormComponent },
+  { path: "does", redirectTo: "/form/create", pathMatch: "prefix" },
+  { path: "table", component: TableComponent },
+  { path: "", redirectTo: "/table", pathMatch: "full" },
   { path: '', component: TableComponent },
+  { path: "**", component: NotFoundComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes, {
