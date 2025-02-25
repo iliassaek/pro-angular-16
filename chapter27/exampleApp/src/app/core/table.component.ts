5389760efd0098594a085a0f2b101891de0f3bf4
iliass <iliassaek.dev@gmail.com>
2025-02-25 14:45:59 +0100

    chore: setup chapter 27

---
+++ chapter27/exampleApp/src/app/core/table.component.ts
@@ -0,0 +1,44 @@
+import { Component, computed, Input, Signal } from '@angular/core';
+import { Product } from '../model/product.model';
+import { Model } from '../model/repository.model';
+
+@Component({
+  standalone: false,
+  selector: 'paTable',
+  templateUrl: 'table.component.html',
+})
+export class TableComponent {
+  @Input()
+  category?: string;
+
+  constructor(private model: Model) {}
+
+  getProduct(key: number): Product | undefined {
+    return this.model.getProduct(key);
+  }
+
+  get Products(): Signal<Product[]> {
+    return computed(() => {
+      return this.model
+        .Products()
+        .filter((p) => this.category == null || p.category == this.category);
+    });
+  }
+
+  get Categories(): Signal<string[]> {
+    return computed(() => {
+      return this.model
+        .Products()
+        .map((p) => p.category)
+        .filter(
+          (c, index, arr) => c != undefined && arr.indexOf(c) == index
+        ) as string[];
+    });
+  }
+
+  deleteProduct(key?: number) {
+    if (key != undefined) {
+      this.model.deleteProduct(key);
+    }
+  }
+}
