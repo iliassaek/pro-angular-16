0f3d427fdbe58eef23151b9a04fcc3d816f828e8
iliass <iliassaek.dev@gmail.com>
2025-02-26 10:07:41 +0100

    chore: setup chapter 28

---
+++ chapter28/exampleApp/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { ModelModule } from './model/model.module';
+import { CoreModule } from './core/core.module';
+import { MessageModule } from './messages/message.module';
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+import { TermsGuard } from './terms.guard';
+import { LoadGuard } from './load.guard';
+import { PlatformService } from './plateform.service';
+import { BrowserGuard } from './browser.guard';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, ModelModule, CoreModule, MessageModule, routing],
+  providers: [
+    TermsGuard,
+    LoadGuard,
+    provideClientHydration(),
+    PlatformService,
+    BrowserGuard,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
