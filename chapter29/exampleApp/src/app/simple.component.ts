5060d4692db43746f8120d8e4121c23bba629e05
iliass <iliassaek.dev@gmail.com>
2025-02-27 11:36:18 +0100

    feat: mock dependencies

--- chapter29/exampleApp/src/app/simple.component.ts
+++ chapter29/exampleApp/src/app/simple.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Model } from './model/repository.model';
+import { Product } from './model/product.model';
 
 @Component({
   standalone: false,
   selector: 'simple',
-  template: `<div class="bg-primary text-white p-2">Unit testing</div>`,
+  template: `<div class="bg-primary text-white p-1">
+    There are
+    <span class="strong"> {{ getProducts().length }} </span>
+    products
+  </div>`,
 })
-export class SimpleComponent {}
+export class SimpleComponent {
+  constructor(private repository: Model) {}
+
+  category: string = 'Soccer';
+
+  getProducts(): Product[] {
+    return this.repository
+      .Products()
+      .filter((p) => p.category == this.category);
+  }
+}
--- chapter29/exampleApp/src/app/tests/simple.component.spec.ts
+++ chapter29/exampleApp/src/app/tests/simple.component.spec.ts
@@ -1,20 +1,36 @@
-import { TestBed, ComponentFixture} from "@angular/core/testing";
-import { SimpleComponent } from "../simple.component";
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { SimpleComponent } from '../simple.component';
+import { Product } from '..//model/product.model';
+import { Model } from '../model/repository.model';
+import { signal } from '@angular/core';
 
-describe("SimpleComponent", () => {
+describe('SimpleComponent', () => {
+  let fixture: ComponentFixture<SimpleComponent>;
+  let component: SimpleComponent;
 
-    let fixture: ComponentFixture<SimpleComponent>;
-    let component: SimpleComponent;
+  let mockRepository = {
+    Products: signal([
+      new Product(1, 'test1', 'Soccer', 100),
+      new Product(2, 'test2', 'Chess', 100),
+      new Product(3, 'test3', 'Soccer', 100),
+    ]),
+  };
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [SimpleComponent]
-        });
-        fixture = TestBed.createComponent(SimpleComponent);
-        component = fixture.componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SimpleComponent],
+      providers: [{ provide: Model, useValue: mockRepository }],
     });
+    fixture = TestBed.createComponent(SimpleComponent);
+    component = fixture.componentInstance;
+  });
 
-    it("is defined", () => {
-        expect(component).toBeDefined()
-    });
+  it('filters categories', () => {
+    component.category = 'Chess';
+    expect(component.getProducts().length).toBe(1);
+    component.category = 'Soccer';
+    expect(component.getProducts().length).toBe(2);
+    component.category = 'Running';
+    expect(component.getProducts().length).toBe(0);
+  });
 });
