8fa89abb1258fb44adfc5c278f2b1db83638b20d
iliass <iliassaek.dev@gmail.com>
2025-02-27 09:09:12 +0100

    feat: rippling effect

--- chapter28/exampleApp/src/app/material.module.ts
+++ chapter28/exampleApp/src/app/material.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSortModule } from "@angular/material/sort";
-import { MatTableModule } from "@angular/material/table";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
- 
-const features = [MatButtonModule, MatTableModule, 
-    MatPaginatorModule, MatSortModule];
- 
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const features = [
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatRippleModule,
+];
+
 @NgModule({
-    imports: [features, BrowserAnimationsModule],
-    exports: [features]
+  imports: [features, BrowserAnimationsModule],
+  exports: [features],
 })
 export class MaterialFeatures {}
