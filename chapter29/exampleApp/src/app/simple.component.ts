41a6b65250e4236fe03622719c5c76e6e572dbf3
iliass <iliassaek.dev@gmail.com>
2025-02-28 11:54:22 +0100

    feat: testing input property

--- chapter29/exampleApp/src/app/simple.component.ts
+++ chapter29/exampleApp/src/app/simple.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+} from '@angular/core';
 import { Model } from './model/repository.model';
 import { Product } from './model/product.model';
 
@@ -8,24 +11,15 @@ import { Product } from './model/product.model';
   templateUrl: 'simple.component.html',
 })
 export class SimpleComponent {
-  constructor(private repository: Model) {}
-
   category: string = 'Soccer';
   highlighted: boolean = false;
 
-  @Output('pa-highlight')
-  change = new EventEmitter<boolean>();
-
   getProducts(): Product[] {
-    return this.repository
-      .Products()
-      .filter((p) => p.category == this.category);
+    return this.model == undefined
+      ? []
+      : this.model.Products().filter((p) => p.category == this.category);
   }
 
-  @HostListener('mouseenter', ['$event.type'])
-  @HostListener('mouseleave', ['$event.type'])
-  setHighlight(type: string) {
-    this.highlighted = type == 'mouseenter';
-    this.change.emit(this.highlighted);
-  }
+  @Input('pa-model')
+  model?: Model;
 }
--- chapter29/exampleApp/src/app/tests/simple.component.spec.ts
+++ chapter29/exampleApp/src/app/tests/simple.component.spec.ts
@@ -2,11 +2,22 @@ import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
 import { SimpleComponent } from '../simple.component';
 import { Product } from '..//model/product.model';
 import { Model } from '../model/repository.model';
-import { signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<simple [pa-model]="model"></simple>`,
+})
+class TestComponent {
+  constructor(public model: Model) {}
+
+  @ViewChild(SimpleComponent)
+  SimpleComponent!: SimpleComponent;
+}
 
 describe('SimpleComponent', () => {
-  let fixture: ComponentFixture<SimpleComponent>;
+  let fixture: ComponentFixture<TestComponent>;
   let component: SimpleComponent;
   let debugElement: DebugElement;
   let divElement: HTMLDivElement;
@@ -21,23 +32,29 @@ describe('SimpleComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [SimpleComponent],
+      declarations: [SimpleComponent, TestComponent],
       providers: [{ provide: Model, useValue: mockRepository }],
     });
     TestBed.compileComponents().then(() => {
-      fixture = TestBed.createComponent(SimpleComponent);
-      component = fixture.componentInstance;
-      debugElement = fixture.debugElement;
-      divElement = debugElement.children[0].nativeElement;
+      fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      component = fixture.componentInstance.SimpleComponent;
+      debugElement = fixture.debugElement.query(By.directive(SimpleComponent));
     });
   }));
 
-  it('implements output property', () => {
-    let highlighted: boolean = false;
-    component.change.subscribe((value) => (highlighted = value));
-    debugElement.triggerEventHandler('mouseenter', new Event('mouseenter'));
-    expect(highlighted).toBeTruthy();
-    debugElement.triggerEventHandler('mouseleave', new Event('mouseleave'));
-    expect(highlighted).toBeFalsy();
+  it('receives the model through an input property', () => {
+    component.category = 'Chess';
+    fixture.detectChanges();
+    let products = mockRepository
+      .Products()
+      .filter((p) => p.category == component.category);
+    let componentProducts = component.getProducts();
+    for (let i = 0; i < componentProducts.length; i++) {
+      expect(componentProducts[i]).toEqual(products[i]);
+    }
+    expect(
+      debugElement.query(By.css('span')).nativeElement.textContent
+    ).toContain(products.length);
   });
 });
